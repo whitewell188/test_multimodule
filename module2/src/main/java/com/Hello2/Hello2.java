f6272d8396339a386b8fa6600cc29485b502f89c
Neal Ward <neal_ward@rapid7.com>
2019-01-18 16:34:16 +0000

    First commit

---
+++ module2/src/main/java/com/Hello2/Hello2.java
@@ -0,0 +1,9 @@
+package com.Hello2;
+
+
+public class Hello2 {
+  public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+  }
+}
